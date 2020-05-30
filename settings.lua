38b9b0925b1ab3a388afae6a4530179e8581c876
Uthrom <>
2020-05-30 16:02:29 -0500

    Commit setting for acid immunity.

--- settings.lua
+++ settings.lua
@@ -14,13 +14,21 @@ data:extend({
     setting_type = "startup",
     localised_name = "Mute drone sounds",
     default_value = false
+[[-]  },{
+    type = "bool-setting",
+    name = "mining_drones_immune_to_acid",
+    order = "ad",
+    setting_type = "startup",
+    localised_name = "Mining drones are immune to acid",
+    default_value = false
+    ]]
   },
   {
     type = "int-setting",
     name = "mining_drone_chance_to_drop_armor_on_death",
-    order = "ad",
+    order = "ae",
     setting_type = "startup",
-    localised_name = "Chance to drop armor on death",
+    localised_name = "Chance for drones to drop armor on death",
     default_value = 100,
     minimum_value = 0,
     maximum_value = 100
